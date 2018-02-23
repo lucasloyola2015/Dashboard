e617fe665d9092429c82da78f585643453e5b7bd
LucioBisiach <l.bisiach@gmail.com>
2018-02-23 17:33:08 -0300

    Cambios

--- src/app/layout/remitos/remitos.module.ts
+++ src/app/layout/remitos/remitos.module.ts
@@ -3,15 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { RemitosRoutingModule } from './remitos-routing.module';
 import { RemitosComponent } from './remitos.component';
-import { AddRemitosComponent } from './add-remitos/add-remitos.component';
-import { TablaRemitosComponent } from './tabla-remitos/tabla-remitos.component';
 
 @NgModule({
   imports: [
     CommonModule,
     RemitosRoutingModule
   ],
-  declarations: [RemitosComponent, AddRemitosComponent, TablaRemitosComponent]
+  declarations: [RemitosComponent]
 })
 export class RemitosModule { }
 
